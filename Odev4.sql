eede11c0250bd1834867db473b0f68ffe36012af
DurmazEda <102880578+DurmazEda@users.noreply.github.com>
2022-11-09 12:44:33 +0300

    Create Odev4.sql

---
+++ Odev4.sql
@@ -0,0 +1,8 @@
+1.) select DISTINCT replacement_cost from film;
+2.) select count (DISTINCT replacement_cost) from film;
+3.) select COUNT(*) from film
+where title LIKE 'T%'and rating = 'G';
+4.) SELECT COUNT(*) FROM country
+WHERE country LIKE '_____';
+5.) SELECT COUNT(*) FROM city
+where city ILIKE '%R'
